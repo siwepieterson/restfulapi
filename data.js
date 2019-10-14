9ec2792728240c45bbe14c26f356286886afe1d6
busipieterson <pietersonbusi@gmail.com>
2019-10-14 22:51:09 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,6 @@
+module.exports = [
+	{ "id": 1, "artist": "Zama Jobe", "song": "Magic"},
+	{ "id": 2, "artist": "Thandiswa Mazwai", "song": "Ngoma"},
+    { "id": 3, "artist": "Simphiwe Dana", "song": "Mayine"},
+    { "id": 4, "artist": "India Arie", "song": "Brown Skin"},
+];
